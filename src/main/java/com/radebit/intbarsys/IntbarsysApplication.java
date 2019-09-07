e1334d763b5ec67b9ce35d4a613b2955724584f7
radebit <26564356@qq.com>
2019-09-07 17:55:51 +0800

    首次提交

---
+++ src/main/java/com/radebit/intbarsys/IntbarsysApplication.java
@@ -0,0 +1,13 @@
+package com.radebit.intbarsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntbarsysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntbarsysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/radebit/intbarsys/IntbarsysApplicationTests.java
@@ -0,0 +1,16 @@
+package com.radebit.intbarsys;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IntbarsysApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
