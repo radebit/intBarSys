a8f085d02d1581b335bd125084ff834c56b96f23
radebit <26564356@qq.com>
2019-09-08 15:51:32 +0800

    字典逻辑

---
+++ src/main/java/com/radebit/intbarsys/model/po/Dictionary.java
@@ -0,0 +1,57 @@
+package com.radebit.intbarsys.model.po;
+
+
+public class Dictionary {
+
+  private Integer id;
+  private String dicKey;
+  private String dicValue;
+  private String dicAttach;
+  private String dicNote;
+
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+
+  public String getDicKey() {
+    return dicKey;
+  }
+
+  public void setDicKey(String dicKey) {
+    this.dicKey = dicKey;
+  }
+
+
+  public String getDicValue() {
+    return dicValue;
+  }
+
+  public void setDicValue(String dicValue) {
+    this.dicValue = dicValue;
+  }
+
+
+  public String getDicAttach() {
+    return dicAttach;
+  }
+
+  public void setDicAttach(String dicAttach) {
+    this.dicAttach = dicAttach;
+  }
+
+
+  public String getDicNote() {
+    return dicNote;
+  }
+
+  public void setDicNote(String dicNote) {
+    this.dicNote = dicNote;
+  }
+
+}
